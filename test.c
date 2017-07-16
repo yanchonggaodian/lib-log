80491c14846ded62a31cb01058148fdec597f427
yanchonggaodian <2223006236@qq.com>
2017-07-17 00:31:27 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "log.h"
+
+int main(int argc, char**argv)
+{
+
+	(void)argc;
+	(void)argv;
+	log_t hnd = module_log_init("TEST", LOG_INFO, NULL, NULL);
+	char*p=NULL;
+	LOG(&p, hnd, LOG_WARN, "ajksj\n");
+	printf("%s",p);
+	LOG(hnd, LOG_WARN, "ipgpqwoir\n");
+
+	return 0;
+}
+
